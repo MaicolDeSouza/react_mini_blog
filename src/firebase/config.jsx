295041b80da670dd12c0f66cb830ceabc1bc0289
Maicol <pessoal.maicol@gmail.com>
2024-08-01 21:22:16 -0300

    put KEYS on .env

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA50vRDJPJ54aaeCC-9rY7DOP8IIDNX0aw",
-  authDomain: "react-miniblog-bb4eb.firebaseapp.com",
-  projectId: "react-miniblog-bb4eb",
-  storageBucket: "react-miniblog-bb4eb.appspot.com",
-  messagingSenderId: "241577656458",
-  appId: "1:241577656458:web:567ee1cb6fc804a224ed73",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
